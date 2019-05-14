8569a042d16aec0d72b9686b21ee582015b28630
Zach Walsh <zcwalsh@ccs.neu.edu>
2019-05-13 22:29:51 -0400

    Configure logging with dictConfig

--- script.py
+++ script.py
@@ -1,17 +1,58 @@
 import sys
+import logging
+import logging.config
 
 import lineup_optimizer
 from espn.espn_api import EspnApi
 from fangraphs_api import FangraphsApi
 from stats import Stats
 
+DEV_LOGGING = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s %(levelname)-8s [%(name)s:%(lineno)d] %(message)s'
+        }
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'espn.api': {
+            'handlers': [],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+
+}
+
+logging.config.dictConfig(DEV_LOGGING)
+
+logger = logging.getLogger()
+
 
 def password(user):
+    """
+    Fetches the user's password from disk.
+    :param str user: the username for which to get the password
+    :return str: the password stored on disk for that user
+    """
     with open(user + "_pass.txt", "w+") as pass_file:
         return pass_file.read()
 
-
 username = sys.argv[1]
+logger.info("starting up with user %(user)s", {"user": username})
 password = password(username)
 
 api = EspnApi(username, password)
