911364503d8570e8461b47185816e0ef05054a4b
Zach Walsh <zach.c.walsh@gmail.com>
2019-06-03 23:41:15 +0000

    Set path variable in scripts so cron will run them

--- fangraphs-check.sh
+++ fangraphs-check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
 export ENV=PROD
 cd "$( dirname ${BASH_SOURCE[0]} )"
 python3.6 check_fg_updated.py
