f2ec89fc6950be1f37f9b448e829709f89ac76f2
Zach Walsh <zcwalsh@ccs.neu.edu>
2019-06-04 12:00:06 -0400

    Fix script.py imports

--- script.py
+++ script.py
@@ -2,23 +2,24 @@ import logging.config
 import sys
 from pathlib import Path
 
-import config.notifier_config
-import config.logging_config
 import optimize.lineup_optimizer
+from config import logging_config
+from config import notifier_config
+from config import password_reader
+from config import team_reader
 from espn.espn_api import EspnApi
 from fangraphs.api import FangraphsApi
 
-
-logging.config.dictConfig(config.logging_config.config_dict())
+logging.config.dictConfig(logging_config.config_dict())
 
 logger = logging.getLogger()
 
 username = sys.argv[1]
 logger.info("starting up with user %(user)s", {"user": username})
-notifier = config.notifier_config.current_notifier(username)
+notifier = notifier_config.current_notifier(username)
 try:
-    password = config.password_reader.password(username, Path.cwd() / "config/passwords")
-    configs = config.team_reader.all_teams(Path.cwd() / "config/team_configs")
+    password = password_reader.password(username, Path.cwd() / "config/passwords")
+    configs = team_reader.all_teams(Path.cwd() / "config/team_configs")
 
     fangraphs = FangraphsApi()
 
