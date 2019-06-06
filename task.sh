f8416b9394f4335ef496cc0d5c55e309dbbaa903
Zach Walsh <zach.c.walsh@gmail.com>
2019-06-06 01:14:46 +0000

    Script that will run a task

---
+++ task.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+export ENV=PROD
+cd "$( dirname ${BASH_SOURCE[0]} )"
+python3.6 run_task.py zcwalsh $1
